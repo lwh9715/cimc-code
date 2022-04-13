a1de19c31f045b31340363332f5813248e4bd153
lwh9715 <lwh9715@gmail.com>
2022-04-13 23:28:11 +0800

    修改起运港目的港输入框

--- cimc-master/pages/system/index.vue
+++ cimc-master/pages/system/index.vue
@@ -3,14 +3,12 @@
 		<uni-card title="FCL报价查询" style="text-align: center;">
 			<uni-forms :modelValue="formData">
 				<uni-forms-item label="起运港">
-					<uni-combox :border="false" emptyTips="匹配不到起运港" :candidates="startlist" placeholder="请输入起运港"
-						v-model="formData.start">
-					</uni-combox>
+					<input style="margin-top: 13rpx;" v-model="formData.start" type="text" placeholder="请输入起运港"
+						@click="openStartPage(0)" />
 				</uni-forms-item>
 				<uni-forms-item label="目的港">
-					<uni-combox :border="false" emptyTips="匹配不到目的港" :candidates="purposelist" placeholder="请输入目的港"
-						v-model="formData.purpose">
-					</uni-combox>
+					<input style="margin-top: 13rpx;" v-model="formData.purpose" type="text" placeholder="请输入起运港"
+						@click="openStartPage(1)" />
 				</uni-forms-item>
 				<uni-forms-item label="出航时间">
 					<view>
@@ -20,14 +18,14 @@
 				</uni-forms-item>
 				<view @click="openCompanyList">
 					<uni-forms-item label="船公司">
-						<view style="margin-top: 20rpx; margin-left: 10rpx;">
+						<view style="margin-top: 16rpx; margin-left: 10rpx;">
 							{{ formData.company }}
 						</view>
 					</uni-forms-item>
 				</view>
 				<view @click="openRoutesList">
 					<uni-forms-item label="航线">
-						<view style="margin-top: 20rpx; margin-left: 10rpx;">
+						<view style="margin-top: 16rpx; margin-left: 10rpx;">
 							{{ formData.routes }}
 						</view>
 					</uni-forms-item>
@@ -59,8 +57,8 @@
 		data() {
 			return {
 				keyword: "",
-				startlist: ['北京', '南京', '东京', '武汉', '天津', '上海', '海口'],
-				purposelist: ['中国', '美国', '巴西', '日本', '中国', '美国', '巴西', '日本'],
+				startlist: ["阿克苏机场", "阿拉山口机场", "阿勒泰机场", "阿里昆莎机场", "安庆天柱山机场", "澳门国际机场"],
+				purposelist: ["保山机场", "包头机场", "北海福成机场", "北京南苑机场", "北京首都国际机场"],
 				companylist: ['中国', '美国', '巴西', '日本', '中国', '美国', '巴西', '日本'],
 				routeslist: ['CNN', 'USD', 'ABX', 'CSS'],
 				formData: {
@@ -91,6 +89,14 @@
 				this.formData.routes = e
 				this.$refs.routesPopup.close()
 			},
+			openStartPage: function(val) {
+				uni.navigateTo({
+					url: '/pages/system/searchBase?id=' + val,
+					fail: (res) => {
+						console.log(res) //打印错误信息
+					}
+				});
+			},
 			/**
 			 * 起运港和目的港信息反转
 			 */
@@ -110,34 +116,15 @@
 						console.log(res) //打印错误信息
 					}
 				});
-				// if (formData == '' || formData == null) {
-				// 	this.formData = []
-				// 	return false
-				// }
-				// let res = await this.$Tools.Axios({
-				// 	url: this.$api.music_search,
-				// 	data: {
-				// 		key: this.keyword
-				// 	},
-				// 	method: 'GET'
-				// });
-				// if (res.result) {
-				// 	this.formData = res.data;
-				// } else {
-				// 	this.formData = []
-				// 	uni.showToast({
-				// 		icon: 'none',
-				// 		title: '请先登录，在进行操作！'
-				// 	})
-				// 	setTimeout(() => {
-				// 		uni.navigateTo({
-				// 			url: '/pages/login/login',
-				// 			fail: (res) => {
-				// 				console.log(res) //打印错误信息
-				// 			}
-				// 		});
-				// 	}, 1000)
-				// }
+			},
+			onShow() {
+				let pages = getCurrentPages();
+				let currPage = pages[pages.length - 1]; // 当前页的实例
+				if (currPage.$vm.index.id == 0) {
+					this.formData.start = currPage.$vm.value
+				} else {
+					this.formData.purpose = currPage.$vm.value
+				}
 			},
 		},
 
@@ -159,7 +146,7 @@
 
 	.scroll-Y {
 		text-align: center;
-		height: calc(100vh - 60vh);
+		height: calc(100vh - 55vh);
 		width: 100%;
 	}
 
---
+++ cimc-master/pages/system/searchBase.vue
@@ -0,0 +1,84 @@
+<template>
+	<view class="card-conent">
+		<uni-indexed-list :options="portlist" :show-select="false" @click="bindClick" />
+	</view>
+</template>
+
+
+<script>
+	export default {
+		data() {
+			return {
+				index: '',
+				portlist: [{
+					"letter": "A",
+					"data": [
+						"阿克苏机场",
+						"阿拉山口机场",
+						"阿勒泰机场",
+						"阿里昆莎机场",
+						"安庆天柱山机场",
+						"澳门国际机场"
+					]
+				}, {
+					"letter": "B",
+					"data": [
+						"保山机场",
+						"包头机场",
+						"北海福成机场",
+						"北京南苑机场",
+						"北京首都国际机场"
+					]
+				}],
+			}
+		},
+		created() {},
+		methods: {
+			bindClick(e) {
+				console.log('点击item，返回数据' + JSON.stringify(e))
+				let pages = getCurrentPages()
+				let nowPage = pages[pages.length - 1]; //当前页页面实例
+				let prevPage = pages[pages.length - 2]; //上一页页面实例
+				console.log(prevPage)
+				prevPage.$vm.index = this.index
+				prevPage.$vm.value = e.item.name
+				uni.navigateBack({
+					delta: 1,
+
+				});
+			},
+			onLoad: function(option) { //option为object类型，会序列化上个页面传递的参数
+				this.index = option
+				console.log("option", option); //打印出上个页面传递的参数。
+			}
+		},
+
+	}
+</script>
+
+<style>
+	.card-conent {
+		height: calc(100vh - 50px);
+		/* background-image: url(../../static/background.png); */
+		overflow-y: auto;
+	}
+
+	.uni-card {
+		margin-top: 100rpx !important;
+		padding: 5px 10px !important;
+		/* border-radius: 15rpx !important; */
+	}
+
+	.scroll-Y {
+		text-align: center;
+		height: calc(100vh - 60vh);
+		width: 100%;
+	}
+
+	.popup-view {
+		text-align: center;
+		margin: 30rpx;
+	}
+
+	.uni-date__icon-logo {}
+</style>
