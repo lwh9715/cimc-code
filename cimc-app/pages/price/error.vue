4734cfc3dcec6c00dcb94c4c0daee6e6b73c4c7b
lwx768561 <8077229+lwx768561@user.noreply.gitee.com>
2022-05-05 18:30:32 +0800

    删除多余页面

---
+++ cimc-app/pages/price/error.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="error-content">
+	</view>
+</template>
+
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		created() {},
+		methods: {}
+
+	}
+</script>
+
+<style>
+	.error-content {
+		height: calc(100vh);
+		background-image: url('../../static/img/404.png');
+		background-repeat: no-repeat;
+		/* 图片按比例显示，其余部分为其他颜色 */
+		background-size: contain;
+	}
+</style>
